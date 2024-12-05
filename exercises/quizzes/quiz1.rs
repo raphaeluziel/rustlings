099a4d7ae7db066396ce1607cf8870171f43621f
raphaeluziel <raphaeluziel@hotmail.com>
2024-12-05 13:49:42 -0500

    done primitives

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -12,6 +12,15 @@
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
 
+fn calculate_price_of_apples(n: i32) -> i32 {
+    if n > 40 {
+        n
+    }
+    else {
+        2 * n
+    }
+}
+
 fn main() {
     // You can optionally experiment here.
 }
